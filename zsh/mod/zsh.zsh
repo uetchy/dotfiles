88228194429f8674adb8df18d7d935549407883b
Yasuaki Uechi <y@uechi.io>
2020-02-16 12:29:08 +0900

    cosmetic changes

--- zsh/mod/zsh.zsh
+++ zsh/mod/zsh.zsh
@@ -68,3 +68,39 @@ zstyle ':completion:*:default' menu select=1 # highlight selection
 
 # commnads
 alias list-commands="compgen -ac | grep '^[^_]'"
+
+# ls
+alias f="open ."
+alias ls="exa --time-style iso"
+alias la="ls -lag --git"
+alias lt="ls -ltchanged"
+alias tree="exa --tree -I node_modules"
+
+# cut
+sel() {
+  cut -d"${2:-' '}" -f"${1:-1}"
+}
+
+# du
+alias volumestat="du -m -x -d 3 $HOME/Repos/src | awk '\$1 >= 500{print}'"
+
+# ff (fast file locate using `find`)
+ff() {
+  local filepath=$(fd -d 3 | fzy)
+  if [[ -n $filepath ]]; then
+    BUFFER=$LBUFFER\"$filepath\"\ $RBUFFER
+  else
+    BUFFER=$LBUFFER$RBUFFER
+  fi
+  CURSOR=$#LBUFFER
+  zle redisplay
+}
+zle -N ff
+bindkey '^f' ff
+
+# pushd
+pds() {
+  local pushd_number=$(dirs -v | fzy | perl -anE 'say $F[0]')
+  [[ -z $pushd_number ]] && return 1
+  pushd +$pushd_number
+}
