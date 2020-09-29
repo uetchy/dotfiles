25a784cdd1d0cc29b52adca76438866bb2396be2
Yasuaki Uechi <y@uechi.io>
2020-09-29 13:50:02 +0900

    update

--- zsh/mod/spaceship.zsh
+++ zsh/mod/spaceship.zsh
@@ -18,7 +18,7 @@ SPACESHIP_PROMPT_ORDER=(
   rust          # Rust section
   # haskell       # Haskell Stack section
   # julia         # Julia section
-  docker        # Docker section
+  # docker        # Docker section
   aws           # Amazon Web Services section
   # gcloud        # Google Cloud Platform section
   # venv          # virtualenv section
