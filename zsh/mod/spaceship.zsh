805698172b713d506c487db979a27cc377a5107e
Yasuaki Uechi <y@uechi.io>
2020-11-26 00:00:53 +0900

    update

--- zsh/mod/spaceship.zsh
+++ zsh/mod/spaceship.zsh
@@ -4,7 +4,7 @@ SPACESHIP_PROMPT_ORDER=(
   # time          # Time stamps section
   # user          # Username section
   dir           # Current directory section
-  # host          # Hostname section
+  host          # Hostname section
   git           # Git section (git_branch + git_status)
   # hg            # Mercurial section (hg_branch  + hg_status)
   # package       # Package version
