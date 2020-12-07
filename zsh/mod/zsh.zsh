e0c4c0e8e372578d72d24c487a2e4ef17d89a081
Yasuaki Uechi <y@uechi.io>
2020-12-07 22:20:19 +0900

    change title in linux

--- zsh/mod/zsh.zsh
+++ zsh/mod/zsh.zsh
@@ -80,3 +80,27 @@ ff() {
 }
 zle -N ff
 bindkey '^f' ff
+
+# title
+change_title() {
+  local title
+  local git_root=$(git rev-parse --show-toplevel 2> /dev/null)
+  if [[ $git_root ]]; then
+    # Check if the parent of the $git_root is "/"
+    if [[ $git_root:h == / ]]; then
+      trunc_prefix=/
+    else
+      trunc_prefix=$SPACESHIP_DIR_TRUNC_PREFIX
+    fi
+    title="$trunc_prefix$git_root:t${${PWD:A}#$~~git_root}"
+  else
+    if [[ $HOME == $PWD ]]; then
+      title="~"
+    else
+      title="${PWD##*/}"
+    fi
+  fi
+  echo -ne "\\033];${title}\\007"
+}
+add-zsh-hook chpwd change_title
+change_title
