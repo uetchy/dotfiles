11bdffa80743fce3847ccbd57b3039109a06d606
Yasuaki Uechi <y@uechi.io>
2021-03-07 18:04:11 +0900

    add si alias

--- zsh/mod/linux.zsh
+++ zsh/mod/linux.zsh
@@ -1,6 +1,7 @@
 alias sctl="systemctl"
 alias jctl="journalctl"
 alias nctl="networkctl"
+alias si="sudo -i"
 
 # Arch
 alias pacman-Qe="comm -23 <(yay -Qe | awk '{print \$1}'|sort) <(yay -Sp base-devel --print-format '%n'|sort)"
